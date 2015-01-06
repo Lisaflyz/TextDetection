5acbd21aabf711c5a83089212e83b50530601af4
hinami <hinami@per900a.hpc.vpl.nii.ac.jp>
2015-01-07 00:19:42 +0900

    upgrade exp env

---
+++ savezip.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+    echo "Usage: $0 date"
+    exit 0
+fi
+
+
+cd res
+for f in *.txt; do 
+    echo $f
+    nkf -Lw $f > buf
+    cat buf | sort | uniq > $f
+done
+zip res_$1.zip *.txt
+
+cd ../
+tar zcf det_$1.tar.gz res/*.txt images/save
+
+mv det_$1.tar.gz ../savedata/TextDetection/
